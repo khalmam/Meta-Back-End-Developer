b6c0ff994a070cde3cb98975e935c0db1113cce0
khalboy <genkhal25@gmail.com>
2024-04-29 11:36:30 +0100

    Used the migrate command to to finish migration, doing such created a migration folder in myapp. Made a change in models.py and made migrations again, adding another file in the migration folder Reverted changes to the first initial model

--- ApplyingMigrations/myapp/models.py
+++ ApplyingMigrations/myapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class MenuItems(models.Model):
-    name = models.CharField(max_length=200)
-    course = models.CharField(max_length=300)
+    itemname = models.CharField(max_length=200)
+    category = models.CharField(max_length=300)
     year = models.IntegerField()
