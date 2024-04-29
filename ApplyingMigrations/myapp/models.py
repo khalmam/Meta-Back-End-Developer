d35321cb9fb1ece128b0ca90f9b45191508143c7
khalboy <genkhal25@gmail.com>
2024-04-29 11:19:39 +0100

    Created a model class in models.py of myapp. Added my app in the list of INSTALLED-APPS in settings.py of ApplyingMigration folder.

--- ApplyingMigrations/myapp/models.py
+++ ApplyingMigrations/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MenuItems(models.Model):
+    name = models.CharField(max_length=200)
+    course = models.CharField(max_length=300)
+    year = models.IntegerField()
