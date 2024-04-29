f8a9336148a5312eb5245e4611acc19da39a7d96
khalboy <genkhal25@gmail.com>
2024-04-29 18:14:44 +0100

    debug an error in views.py and home.html

--- CreatingForms/myapp/forms.py
+++ CreatingForms/myapp/forms.py
@@ -7,7 +7,7 @@ SHIFTS = (
 )
 
 
-class InputForm(forms.form):
+class InputForm(forms.Form):
     first_name = forms.CharField(max_length=200)
     last_name = forms.CharField(max_length=200)
     shift = forms.ChoiceField(choices=SHIFTS)
