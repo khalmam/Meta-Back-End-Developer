c50e6c2340aa7643084b67ee03e1fd25bfadf033
khalboy <genkhal25@gmail.com>
2024-04-29 18:00:35 +0100

    Updated the necessary code in views.py

--- CreatingForms/myapp/views.py
+++ CreatingForms/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from myapp.forms import InputForm
 # Create your views here.
+
+def formview(request):
+    form = InputForm()
+    context = {"form": form}
+    return render(request,"home.html", context)
