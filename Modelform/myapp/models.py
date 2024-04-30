d04adac1844aee64b1c5540f4003ad9cc58e401b
khalboy <genkhal25@gmail.com>
2024-04-30 12:07:39 +0100

    Created the forms.py file and added the necessary code. Updated the models.py file with the necessary code.

--- Modelform/myapp/models.py
+++ Modelform/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Logger(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    time_log = models.TimeField()
