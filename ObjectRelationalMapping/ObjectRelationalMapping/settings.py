3763382d4f62ae18b320360b7421349954a8eab9
khalboy <genkhal25@gmail.com>
2024-04-30 13:57:29 +0100

    Updated the necessary code in the models.py Registered the model in admin.py Updated code in views.py

--- ObjectRelationalMapping/ObjectRelationalMapping/settings.py
+++ ObjectRelationalMapping/ObjectRelationalMapping/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-     'demoapp.apps.DemoappConfig',
+    'demoapp.apps.DemoappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
