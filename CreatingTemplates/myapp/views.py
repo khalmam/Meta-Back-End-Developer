e0853eb7b434bd8857f97ebff371991fcccf68e3
khalboy <genkhal25@gmail.com>
2024-05-11 21:08:33 +0100

    debugged some errors

--- CreatingTemplates/myapp/views.py
+++ CreatingTemplates/myapp/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 
 def about(request):
     about_content = {'about': 'We eat with our eyes first, but before we see our food, we picture it while reading the menu descriptions. They say one image is worth a thousand words, but don’t underestimate the power of words. A few strategically placed words here and there can make your food and wine descriptions increase your restaurant’s sales'}
-    return render(request, "about.html", about_content)
+    return render(request, "about.html",{'content':about_content})
+
 
 def menu(request):
     about_content = {'about': 'We eat with our eyes first, but before we see our food, we picture it while reading the menu descriptions. They say one image is worth a thousand words, but don’t underestimate the power of words. A few strategically placed words here and there can make your food and wine descriptions increase your restaurant’s sales'}
-    return render(request, "about.html", about_content)
+    return render(request, "menu.html",about_content)
 
 #def form_view(request):
  #   form = BookingForm()
