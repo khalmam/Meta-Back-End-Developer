7b1675f7309ec0e168cc28dc5062d2086f6a4abd
khalboy <genkhal25@gmail.com>
2024-05-12 20:32:10 +0100

    debugged an error

--- WorkingWithTemplates/myproject/settings.py
+++ WorkingWithTemplates/myproject/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-STATICFILES_DIRS = ["myproject/static",]
+STATICFILES_DIRS = ["myapp/static",]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
