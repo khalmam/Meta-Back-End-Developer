521b7a67b2ad5f486c6e163ae11b55787b004914
khalboy <genkhal25@gmail.com>
2024-06-12 14:17:04 +0100

    edited settings.py and added 'rest_framework.authtoken'

--- UserAccount/LittleLemon/settings.py
+++ UserAccount/LittleLemon/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'LittleLemonDRF',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     'djoser',
 ]
 
