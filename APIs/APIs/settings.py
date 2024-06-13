6a7964f3c82b952f6c8d896eee1a2ade02edf668
khalboy <genkhal25@gmail.com>
2024-06-13 15:07:56 +0100

    Updated the INSTALLED_APPS section of settings.py with the necessary apps Added rest_framework and Djoser configurations

--- APIs/APIs/settings.py
+++ APIs/APIs/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'LittleLemonAPI',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
