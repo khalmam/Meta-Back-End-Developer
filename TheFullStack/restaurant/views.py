7806cb0bcc1f0a4ae823cebece4102ca7549b10d
khalboy <genkhal25@gmail.com>
2024-07-06 15:35:44 +0100

    bug fix

--- TheFullStack/restaurant/views.py
+++ TheFullStack/restaurant/views.py
@@ -28,7 +28,9 @@ def bookings(request):
     date = request.GET.get("date", datetime.today().date)
     bookings = Booking.objects.all()
     booking_json = serializers.serialize("json", bookings)
-
+    
+    #return render(request, "bookings.html", {"bookings": booking_json})
+    
     bookings_data = {"bookings": bookings}
     return render(request, "bookings.html", {"bookings": bookings_data})
 
